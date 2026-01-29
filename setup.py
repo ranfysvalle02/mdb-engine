c4f0b0142f1205a53667b79df6692f28cb71c982
Fabian Valle <ranfys.valle@gmail.com>
2026-01-29 18:23:19 -0500

    fastapi app injection for multi-app+++++v0.3.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ if readme_file.exists():
 
 setup(
     name="mdb-engine",
-    version="0.2.4",
+    version="0.3.0",
     description="MongoDB Engine",
     long_description=long_description,
     long_description_content_type="text/markdown",
